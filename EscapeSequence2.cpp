92de9ad9834d0d7753497927e56e218ee3752809
saint-kyene <onyx.kyene@strathmore.edu>
2023-10-05 10:16:46 -0700

    escape sequence 3

---
+++ EscapeSequence2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+    cout <<"*****\"Escape sequence practice\"*****\n";
+    cout<< "Dbit is a universit ydiploma desgined to focus on \n";
+    cout<<"\t1.development \n";
+    cout <<"\t2.implementation,and \n";
+    cout <<"\t3.management of information system.";
+    return 0;
+
+}
---
+++ EscapeSequence3.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{cout <<"Stick woman and man standing next to\n";
+cout <<"their car.\n";
+cout <<"---------------------------------------";
+//Display stick man 
+    std::cout <<"\t  ______-" << std::endl;
+    std::cout <<"\t |--  --|" << std::endl;
+    std::cout <<"\t |  --  |" << std::endl;
+    std::cout <<"\t  ------" << std::endl;
+    std::cout <<"\t     |" <<std::endl;
+    std::cout <<"\t     |" <<std::endl;
+    std::cout <<"\t    /\\" <<std::endl;
+    std::cout <<"\t   /  \\"<<std::endl;
+}
+    
+#include <iostream>
+
+int main ()
+{
+    // Display the leveled ground
+    std::cout << "   ________" << std::endl;
+    std::cout << "  /       /\\ " << std::endl;
+    std::cout << " /_______/  \\ " << std::endl;
+
+    // Display the stick man and woman
+    std::cout << "  O    O" << std::endl;
+    std::cout << "   \\  /" << std::endl;
+    std::cout << "    |" << std::endl;
+    std::cout << "   / \\" << std::endl;
+    std::cout << "  /   \\" << std::endl;
+
+    // Display the car
+    std::cout << "\t  ______" << std::endl;
+    std::cout << "\t /|_||_\\`.__" << std::endl;
+    std::cout << "\t(   _    _ _)" << std::endl;
+    std::cout << "\t=`-(_)--(_)-'" << std::endl;
+
+
+    return 0;
+
+}
